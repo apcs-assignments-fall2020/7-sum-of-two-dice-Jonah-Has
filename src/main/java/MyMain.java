3259bf769c58cac59b9684d1b5cf29993c0d8519
Jonah-Has <23jhasegawa@athenian.org>
2020-09-28 03:54:43 +0000

    wrote the code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,24 +1,44 @@
 import java.util.Scanner;
-
 public class MyMain {
-
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        int x = (int) (Math.random() * 6);
+        return x + 1;
     }
-
     // Rolls two dice n times
+
+    // -- in the README you said to roll n dice, but I assume this was a mistake.
+
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] returners;
+        returners = new int[11];
+        int[] results;
+        results = new int[n];
+        for (int i = 0; i < results.length; i++){
+            results[i] = rollDie() + rollDie();
+            for (int j = 0; j < returners.length; j++){
+                if (results[i] == j + 2){
+                    returners[j] += 1;
+                }
+            }
+        }
+        return returners;
     }
 
-
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many pairs of dice would you like to roll?");
+        int x = scan.nextInt();
+        int[] printers = sumOfTwoDice(x);
+
+        for (int i = 0; i < printers.length; i++){
+            
+            double p = (printers[i] / ((double) x )) / 100.0;
+            System.out.println("% of rolls where sum == " + (i + 2) + ": " + p);
+        }
+        scan.close();
     }
 }
